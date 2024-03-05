26884d48734941009d386735f0d99a655702bf8c
DepressedPenguin <125665615+DepressedPenguin@users.noreply.github.com>
2024-03-05 19:48:37 +0100

    add some things

--- src/Components/Guide/index.jsx
+++ src/Components/Guide/index.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./style.css";
 
+import img_1 from "../guide_imgs/1.png";
+import img_2 from "../guide_imgs/2.png";
+
 export default function Guide() {
   return (
     <>
@@ -15,10 +18,24 @@ export default function Guide() {
               <div className="title_pic">
                 <p>1-BRING ANY URL FROM YOUTUBE</p>
               </div>
-              <div className="pic_guide"></div>
+              <div className="pic_guide">
+                <img src={img_1}></img>
+              </div>
             </div>
-            <div className="box_show"></div>
-            <div className="box_show"></div>
+            <div className="box_show">
+              <div className="title_pic">
+                <p>1-CLICK CONVERT AND WAIT A FEW SECONDS</p>
+              </div>
+              <div className="pic_guide">
+                <img src={img_2}></img>
+              </div>
+            </div>
+            <div className="box_show">
+              <div className="title_pic">
+                <p>1-CLICK CONVERT AND WAIT A FEW SECONDS</p>
+              </div>
+              <div className="pic_guide">{/* <img src={img_2}></img> */}</div>
+            </div>
           </div>
         </div>
         <div className="social_media"></div>
